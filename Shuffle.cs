d4d14b0faa3380226a976c9300c1d7bb5cdaaa17
RiccardoDori <riccardo.dori@avanade.com>
2020-06-18 10:07:38 +0200

    Add files via upload

---
+++ Shuffle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DUL.Utilities
+{
+    public class Shuffle
+    {
+        private static Random R = new Random();
+        public static T GiveMeOne<T>(params T[] fromThis)
+        {
+            return fromThis[R.Next(fromThis.Count())];
+        }
+    }
+}
